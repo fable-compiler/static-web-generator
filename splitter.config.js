ae2067daa14b0f46b8c92fd20f665632c761d569
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-12-11 13:16:19 +0100

    Use fork to run the script

--- splitter.config.js
+++ splitter.config.js
@@ -6,21 +6,33 @@ function resolve(filePath) {
 }
 
 function runScript(scriptPath) {
-  var scriptDir = path.dirname(scriptPath);
-  // Delete files in directory from require cache
-  Object.keys(require.cache).forEach(function(key) {
-    if (key.startsWith(scriptDir))
-      delete require.cache[key]
-  })
-  require(scriptPath);
+  try {
+      console.log("Running script")
+      var childProcess = require("child_process");
+      var path = require("path");
+      var cp = childProcess.fork(scriptPath);
+      cp.on("exit", function (code, signal) {
+          if (code === 0) {
+              console.log("Success");
+          } else {
+              console.log("Exit", { code: code, signal: signal });
+          }
+      });
+      cp.on("error", console.error.bind(console));
+  } catch (err) {
+      console.error(err);
+  }
 }
 
 var outFile = resolve("build/Main.js");
+
+var isProduction = process.argv.indexOf("-p") >= 0;
+console.log("[Environment]: " + (isProduction ? "production" : "development"));
 
 module.exports = {
   entry: resolve("src/Fable.StaticPageGenerator.fsproj"),
   outDir: path.dirname(outFile),
   babel: { plugins: ["transform-es2015-modules-commonjs"] },
-  fable: { define: ["DEBUG"] },
+  fable: { define: isProduction ? [] : ["DEBUG"], },
   postbuild() { runScript(outFile) }
 };
